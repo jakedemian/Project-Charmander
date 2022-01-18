425b39a835bc94341340772bc929be2b303d453e
Jake Demian <jakedemian@gmail.com>
2022-01-18 00:12:56 -0500

    swapped player movement to a navigation map based movement system

--- Assets/_Scripts/Controllers/PlayerController.cs
+++ Assets/_Scripts/Controllers/PlayerController.cs
@@ -1,48 +1,21 @@
 using _Scripts.Types;
-using _Scripts.Util;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
     public LayerMask groundLayer;
     public float moveSpeed;
 
     private NullableVector3 currentMoveTarget;
-    private Rigidbody rb;
+    private NavMeshAgent navMeshAgent;
 
     private void Start() {
-        rb = GetComponent<Rigidbody>();
         currentMoveTarget = new NullableVector3();
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void Update() {
         UpdateMoveTarget();
-        MoveTowardsTarget();
-    }
-
-    private void MoveTowardsTarget() {
-        if (!currentMoveTarget.Exists()) {
-            return;
-        }
-
-        var playerPos = transform.position;
-        var playerPos2D = new Vector2(playerPos.x, playerPos.z);
-        var targetPos = currentMoveTarget.Get();
-        var targetPos2D = new Vector2(targetPos.x, targetPos.z);
-        if (Vector2.Distance(playerPos2D, targetPos2D) < 0.5f) {
-            currentMoveTarget.Clear();
-            transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, 0f);
-            rb.velocity = Vector3.zero;
-            return;
-        }
-
-
-        // bug to note, if you continuously path into a wall, your y value slowly raises
-        var towardsTarget = targetPos - playerPos;
-        var directionVector = new Vector3(towardsTarget.x, 0f, towardsTarget.z).normalized;
-        rb.velocity = directionVector * moveSpeed;
-
-        transform.LookAt(new Vector3(targetPos.x, 0, targetPos.z));
-        transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, 0f);
     }
 
     private void UpdateMoveTarget() {
@@ -51,10 +24,7 @@ public class PlayerController : MonoBehaviour {
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity,
                     groundLayer)) {
                 currentMoveTarget.Set(hit.point);
-
-                if (currentMoveTarget.Exists()) {
-                    JLog.Log(currentMoveTarget.Get());
-                }
+                navMeshAgent.SetDestination(currentMoveTarget.Get());
             }
         }
     }
